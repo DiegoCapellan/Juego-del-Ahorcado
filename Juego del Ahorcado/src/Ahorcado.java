ecd7faa2647d51a4ae16d68a3cad95b0caab1d36
alumno <alumno@aula1-12>
2019-10-29 11:20:26 +0100

    Inicio del Proyecto

---
+++ Juego del Ahorcado/src/Ahorcado.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Ahorcado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner leer = new Scanner(System.in);
+		
+		System.out.println(" ---------------------");
+        System.out.println(" |                     |");
+        System.out.println(" |                     |");
+        System.out.println(" |                  -------");
+        System.out.println(" |                 |  -  - |");
+        System.out.println(" |                 |   o   |");
+        System.out.println(" |                  -------");
+        System.out.println(" |                     |   ");
+        System.out.println(" |                   / | \\ ");
+        System.out.println(" |                  /  |   \\ ");
+        System.out.println(" |                 /   |     \\ ");
+        System.out.println(" |                     |   ");
+        System.out.println(" |                    / \\");
+        System.out.println(" |                   /   \\  ");
+        System.out.println(" |                  /     \\ ");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
